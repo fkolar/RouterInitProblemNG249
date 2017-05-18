6ee7779b1ab494dd1089ee76e3cd6816f94a1122
Frantisek kolar <frank.kolar@sap.com>
2017-05-17 20:40:53 -0700

    sss

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import {AppRoutingModule} from "./app-routing.module";
+import {MyCoreModule} from "./core/my-core.module";
+import {TestAppModule} from "./test/test-app.module";
 
 @NgModule({
   declarations: [
@@ -12,9 +15,18 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AppRoutingModule,
+    TestAppModule, // import actuall app
+    MyCoreModule // import core as we are injecting RoutingService into AppComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+
+  constructor() {
+    console.log("Constructor AppModule")
+  }
+}
